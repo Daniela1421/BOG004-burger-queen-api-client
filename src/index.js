3cc150380121ba5177ac6f5f530fb01fedaaead3
Daniela1421 <daniela.ducuara1817@gmail.com>
2022-05-24 21:18:32 -0500

    arreglo de create-react-app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-ReactDOM.render(<App />, document.getElementById('root'))
 
