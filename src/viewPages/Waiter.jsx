1b10c3bb7a2763bb8a903680192e657d130a5f42
María Paula Quintero Rodriguez <79113076+PaulaQR@users.noreply.github.com>
2022-06-02 13:01:41 -0500

    Petición de productos

--- src/viewPages/Waiter.jsx
+++ src/viewPages/Waiter.jsx
@@ -1,12 +1,55 @@
 import Logout from "../components/Logout";
 import Restaurant from "../components/Restaurant";
+import React, { useState} from 'react';
 
 export default function Waiter() {
+  const [products, setProducts] = useState('')
+  const urlApi = 'http://localhost:8080';
+
+  const validateHttpProducts = () => {
+
+    fetch(`${urlApi}/products`, {
+      method: 'GET', // or 'PUT'
+      //body: JSON.stringify(data), // data can be `string` or {object}!
+      headers:{
+        'Content-Type': 'application/json',
+        'authorization': 'Bearer ' + localStorage.getItem('userToken'),
+      }
+
+    })
+    
+   .then(res => res.json())
+   
+   .then(data => {
+     /*
+     data.forEach((products) => {
+       products.quantity = 0;
+     })
+     if (typeof data === 'object' && data.length > 0){
+       */
+    data.forEach((products) => {
+     if(products.type === 'Desayuno'){
+       setProducts(products)
+     }
+    })
+       setProducts(data);
+     
+
+     console.log('Success:', data)
+   })
+    
+   .catch(error => console.error('Error:', error))
+  }
+
+  const handleClick = () => {
+    validateHttpProducts()
+  }
+  
   return (
       <div className='waiter'>
         <header className="header">
         <div className="menu">
-          <button className='input-buttons' id="breakfast"/*onClick={handleClick}*/>BREAKFAST</button>
+          <button className='input-buttons' id="breakfast" onClick={handleClick} >BREAKFAST</button>
           <button className='input-buttons' id="today-menu"/*onClick={handleClick}*/>TODAY'S MENU</button>
         </div>
         <Restaurant/>
@@ -22,6 +65,7 @@ export default function Waiter() {
           </div>
           <div>
           <input className='user-name' type="text" placeholder="Enter customer name"/*value={email} onChange={(e) => { setEmail(e.target.value);}}*/ />
+          <h2> {products} </h2>
           </div>
         </section>
         <section>
